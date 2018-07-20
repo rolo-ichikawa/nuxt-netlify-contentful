d3f9bd7a4f8b012f8026ba688ffa2e5282a3681f
rolo-ichikawa <d.ichikawa@rolo.jp>
2018-07-20 11:15:57 +0900

    feat nuxtにcontentfuulの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,10 @@
+const contentful = require('contentful')
+const config = require('./.contentful.json')
+const client = contentful.createClient({
+  space: config.CTF_SPACE_ID,
+  accessToken: config.CTF_CDA_ACCESS_TOKEN
+})
+
 module.exports = {
   /*
   ** Headers of the page
@@ -34,6 +41,11 @@ module.exports = {
         })
       }
     }
+  },
+  env: {
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: config.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
   }
 }
-
